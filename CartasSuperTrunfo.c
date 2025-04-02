72c5729677e77c2a72b419b3d769f4b56deb98b2
Daoifyz <bfsfernandes126@gmail.com>
2025-04-02 13:27:16 +0000

    Código pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,112 @@
 #include <stdio.h>
+int main (){
+//dados das cartas
+char estado1;
+char estado2;
+char codigo1[10];
+char codigo2[10];
+int populacao1;
+int populacao2;
+char nomedacidade1[20];
+char nomedacidade2[20];
+float area1;
+float area2;
+float pib1;
+float pib2;
+int pontosturisticos1;
+int pontosturisticos2;
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+//entrada de dados da primeira carta
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+printf("Escolha uma letra de A a H para ser o Estado. \n");
+printf("Estado: ");
+scanf(" %c", &estado1);
 
-    return 0;
+printf("Coloque a letra do estado seguido de 1 número de 01 a 04 para ser o código da carta. \n");
+printf("Código: ");
+scanf(" %2s", codigo1);
+
+printf("Insira a quantidade de habitantes da cidade\n");
+printf("População: ");
+scanf(" %d", &populacao1);
+
+printf("Escreva o nome da cidade. \n");
+getchar();
+printf("Nome da cidade: ");
+fgets(nomedacidade1, sizeof(nomedacidade1), stdin);
+
+printf("Insira a área territorial da cidade. \n");
+printf("Área em km²: ");
+scanf(" %f", &area1);
+
+printf("Insira o PIB da cidade \n");
+printf("PIB: ");
+scanf("%f", &pib1);
+
+printf("Insira a quantidade de pontos turísticos da cidade. \n"); 
+printf("Pontos turísticos: ");
+scanf(" %d", &pontosturisticos1);
+
+// dados da segunda carta
+
+printf("Escolha uma letra de A a H para ser o Estado 2. \n");
+printf("Estado: ");
+scanf(" %c", &estado2);
+
+printf("Coloque a letra do estado seguido de 1 número de 01 a 04 para ser o código da carta. \n");
+printf("Código: ");
+scanf(" %2s", codigo2);
+
+printf("Insira a quantidade de habitantes da cidade\n");
+printf("População: ");
+scanf(" %d", &populacao2);
+
+printf("Escreva o nome da cidade. \n");
+getchar();
+printf("Nome da cidade: ");
+fgets(nomedacidade2, sizeof(nomedacidade2), stdin);
+
+printf("Insira a área territorial da cidade. \n");
+printf("Área em km²: ");
+scanf(" %f", &area2);
+
+printf("Insira o PIB da cidade \n");
+printf("PIB: ");
+scanf("%f", &pib2);
+
+printf("Insira a quantidade de pontos turísticos da cidade. \n"); 
+printf("Pontos turísticos: ");
+scanf(" %d", &pontosturisticos2);
+
+//mostrando dados da carta 
+
+printf("\n═════════════════════════════════════════\n");
+printf("             CARTA 1                      \n");
+printf("═════════════════════════════════════════\n");
+
+printf("Estado: '%c' \n", estado1);
+printf("Código: %c%2s\n", estado1, codigo1);
+printf("Nome da cidade: %s \n", nomedacidade1);
+printf("População: %d \n", populacao1);
+printf("Área: %2.f km² \n", area1);
+printf("PIB: %.2f bilhões de reais\n", pib1);
+printf("Ptontos turísticos: %d\n", pontosturisticos1);
+
+printf("\n═════════════════════════════════════════\n");
+printf("             CARTA 2                      \n");
+printf("═════════════════════════════════════════\n");
+
+printf("Estado: '%c' \n", estado2);
+printf("Código: %c%2s\n", estado2, codigo2);
+printf("Nome da cidade: %s \n", nomedacidade2);
+printf("População: %d \n", populacao2);
+printf("Área: %2.f km² \n", area2);
+printf("PIB: %.2f bilhões de reais\n", pib2);
+printf("Ptontos turísticos: %d\n", pontosturisticos2);
+
+return 0;
+
 }
+
+
+
